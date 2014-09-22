bf7fef961465de96faa8f9ee8aaf5806f68e89cf
idsc3101 <idsc3101@gmail.com>
2014-09-22 11:24:16 -0500

    Created the project skeletons

---
+++ IfElse/IfElse/Form1.vb
@@ -0,0 +1,30 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        Dim num1, num2, largerNum As Double
+
+        ' Modify the program such that it does not crash 
+        ' if the user types incorrect input
+
+        Dim st1 As String = txtFirstNum.Text
+        Dim st2 As String = txtSecondNum.Text
+
+        num1 = CDbl(st1)
+        num2 = CDbl(st2)
+
+        Dim result As String
+
+        If num1 > num2 Then
+            result = "The larger number is " & num1
+        ElseIf num2 > num1 Then
+            result = "The larger number is " & num2
+        Else
+            result = "They are equal"
+        End If
+
+        txtResult.Text = result
+    End Sub
+
+End Class
